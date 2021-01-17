94bdf947b42a34f89e168a651cc6fe6a7bef8d85
Nza6920 <1484663282@qq.com>
2021-01-17 18:43:55 +0800

    配置相关参数

--- websocket-stomp/src/main/java/com/niu/websocket/config/WebSocketConfig.java
+++ websocket-stomp/src/main/java/com/niu/websocket/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.niu.websocket.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -22,5 +23,17 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/websocket")
                 .setAllowedOrigins("*")
                 .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 配置允许的代理前缀
+        registry.enableSimpleBroker("/topic/", "/queue/", "/user/");
+
+        // 配置点对点发送前缀
+//        registry.setUserDestinationPrefix("/user/");
+
+        // 配置客户端前缀
+//        registry.setApplicationDestinationPrefixes("/app");
     }
 }
