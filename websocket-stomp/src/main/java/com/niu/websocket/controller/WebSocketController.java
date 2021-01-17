bba3364273eb43fdfb45bb4ffb923f14d7d194a6
Nza6920 <1484663282@qq.com>
2021-01-17 18:26:18 +0800

    stomp 点对点发送

--- websocket-stomp/src/main/java/com/niu/websocket/controller/WebSocketController.java
+++ websocket-stomp/src/main/java/com/niu/websocket/controller/WebSocketController.java
@@ -1,6 +1,5 @@
 package com.niu.websocket.controller;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -8,6 +7,8 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * WebSocket 控制器
@@ -22,7 +23,6 @@ public class WebSocketController {
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
 
-
     /**
      * 支持 websocket 调用此方法
      */
@@ -32,13 +32,33 @@ public class WebSocketController {
         return message;
     }
 
-
     /**
      * 自动降级将调用此方法
      */
     @GetMapping("/send")
-    public String msgReply(@RequestParam String msg) {
+    public void topicMsgReply(@RequestParam String msg) {
         simpMessagingTemplate.convertAndSend("/topic", msg);
-        return msg;
+    }
+
+    /**
+     * 群发消息
+     */
+    @MessageMapping("/send2All")
+    @GetMapping("/send2All")
+    public void handleSend2All(String message) {
+        simpMessagingTemplate.convertAndSend("/send2All", message);
+    }
+
+    /**
+     * 点对点发送
+     */
+    @MessageMapping("/send2User")
+    public void handleSend2User(Map<String, String> message) {
+
+        String fromUserId = message.get("fromUserId");
+        String toUserId = message.get("toUserId");
+        String msg = "来自: " + fromUserId + " 的消息: " + message.get("msg");
+
+        simpMessagingTemplate.convertAndSendToUser(toUserId, "/topic", msg);
     }
 }
--- websocket-stomp/src/main/resources/static/js/websocket.js
+++ websocket-stomp/src/main/resources/static/js/websocket.js
@@ -1,37 +1,57 @@
 var stompClient = null;
 var wsCreateHandler = null;
+var userId = null;
 
 function connect() {
-	var host = window.location.host; // 带有端口号
-	var socket = new SockJS("http://" + host + "/websocket");
-	stompClient = Stomp.over(socket);
-	stompClient.connect({}, function (frame) {
-		writeToScreen("connected: " + frame);
-		stompClient.subscribe('/topic', function (response) {
-			writeToScreen(response.body);
-		});
+    var host = window.location.host; // 带有端口号
+    userId = GetQueryString("userId");
+    var socket = new SockJS("http://" + host + "/websocket");
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+            writeToScreen("connected: " + frame);
+            stompClient.subscribe('/topic', function (response) {
+                writeToScreen("主题: topic, 消息: " + response.body);
+            });
 
-		}, function (error) {
-			wsCreateHandler && clearTimeout(wsCreateHandler);
-			wsCreateHandler = setTimeout(function () {
-				console.log("重连...");
-				connect();
-				console.log("重连完成");
-			}, 1000);
-		}
-	)
+            stompClient.subscribe('/send2All', function (response) {
+                writeToScreen("主题: send2All, 消息: " + response.body);
+            });
+
+            stompClient.subscribe('/user/' + userId + "/topic", function (response) {
+                writeToScreen("主题: user, 消息: " + response.body);
+            });
+
+        }, function (error) {
+            wsCreateHandler && clearTimeout(wsCreateHandler);
+            wsCreateHandler = setTimeout(function () {
+                console.log("重连...");
+                connect();
+                console.log("重连完成");
+            }, 1000);
+        }
+    )
 }
 
 function disconnect() {
-	if (stompClient != null) {
-		stompClient.disconnect();
-	}
-	writeToScreen("disconnected");
+    if (stompClient != null) {
+        stompClient.disconnect();
+    }
+    writeToScreen("disconnected");
 }
 
 function writeToScreen(message) {
-	if(DEBUG_FLAG)
-	{
-		$("#debuggerInfo").val($("#debuggerInfo").val() + "\n" + message);
-	}
+    if (DEBUG_FLAG) {
+        $("#debuggerInfo").val($("#debuggerInfo").val() + "\n" + message);
+    }
+}
+
+function GetQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg); //获取url中"?"符后的字符串并正则匹配
+    var context = "";
+    if (r != null)
+        context = r[2];
+    reg = null;
+    r = null;
+    return context == null || context == "" || context == "undefined" ? "" : context;
 }
